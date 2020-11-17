a3aaa3d6da4fef6e1fc12604076ef4a77cc9a007
chensongtao <chensongtao@9fbank.com.cn>
2020-11-17 16:08:16 +0800

    实现 map

---
+++ leetCode/_map.js
@@ -0,0 +1,24 @@
+/**
+ * 实现 map
+ */
+
+Object.prototype._map = function(fn, arg1) {
+    if(typeof fn != 'function') {
+        throw new TypeError(`${fn} is not a function`)
+    }
+    return JSON.stringify(this, (key, val) => {
+        if(key) {
+            return fn.call(arg1, key, val, this)
+        } else {
+            return val;
+        }
+    })
+}
+// 用例
+let obj = {
+    a: 2, b: 3, c: 4, d: 5
+}
+
+let _obj = obj._map((key, val) => val + key + p,  p = 1)
+
+console.log(_obj)
