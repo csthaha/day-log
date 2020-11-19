f98cd80269eacf297b2b36c35071def61196b766
chensongtao <chensongtao@9fbank.com.cn>
2020-11-19 11:35:44 +0800

    proxy操作对象

--- day-knowledge/11-19.js
+++ day-knowledge/11-19.js
@@ -35,3 +35,40 @@ if ([0]) {
 // * 只有一个元素时，根据该元素进行 Number 转换
 
 // 所有的 对象 转化为 Number 时，都为 NaN
+
+/**
+ * 3  Proxy 用来自定义对象中的操作
+ */
+
+ const handler = {
+     //get运算符有两个参数 - 对象本身和被访问的属性。
+     get: function(obj, prop) {
+         if(prop == 'id') {
+             throw new Error('Cannot access private properties!')
+         } else {
+            console.log('this is handler.get')
+            return obj[prop]   // 返回访问的key在obj的值
+         }
+     },
+     //set运算符有3个参数 - 对象本身,被访问的属性, 修改的值。
+     set: function(obj, prop, value) {
+        if(typeof value !== 'string') {
+            throw new Error('Only string values can be stored in this object!');
+        } else {
+            console.log(`${prop} is being set to ${value}`);
+            obj[prop] = value;
+        }
+     }
+ }
+
+ const initObj = {
+     id: 1,
+     name: "Foo Bar"
+ }
+
+ const proxyObj = new  Proxy(initObj, handler)
+ console.log('proxy obj', proxyObj.name)
+//  console.log('proxy obj.id: ', proxyObj.id)
+
+ proxyObj.age = 'twenty Four'
+ console.log('proxy obj.age: ', proxyObj.age)
