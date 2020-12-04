4f419de5976e44f123714c8766964244fe058518
chensongtao <chensongtao@9fbank.com.cn>
2020-12-04 10:57:03 +0800

    12-4 day-log

---
+++ day-knowledge/12-4.js
@@ -0,0 +1,18 @@
+/**
+ * T 30
+ */
+const obj = {
+    "2": 3,
+    "3": 4,
+    length: 2,
+    splice: Array.prototype.splice,
+    push: Array.prototype.push,
+  };
+  obj.push(1);
+  obj.push(2);
+  console.log(obj);
+
+  /**
+   * 
+   */
+
